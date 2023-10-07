8b671d0eede7d624956f314fba62eaa82bf46ab1
Liam <143908710+liamperfil@users.noreply.github.com>
2023-10-07 13:27:36 -0300

    Update query builder.sql

--- query builder.sql
+++ query builder.sql
@@ -1,4 +1,8 @@
-SELECT id, codigo, SUM(quantidade) as soma_quantidade
-FROM estoque_copy1
+/*  */
+SELECT id, codigo, descricao, ncm, MAX(cst) as cst_, cfop, un, SUM(quantidade) as soma_, 
+MAX(valor) as valor_, MAX(ean) as ean_, emit
+FROM estoque
 GROUP BY codigo, descricao
 ORDER BY descricao ASC;
+
+SELECT * FROM estoque WHERE codigo='1253';
